96f77694de1dc1857d3ff67260fb0c544b8405f3
Dev - Fabio Freitas <32686962+devfabiofreitas@users.noreply.github.com>
2022-01-03 18:25:13 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -56,7 +56,6 @@ defmodule Crosschallenge.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:httpoison, "~> 1.8"},
       {:tesla, "~> 1.4"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:hackney, "~> 1.17"},
